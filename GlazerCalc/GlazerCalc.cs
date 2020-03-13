def0f25a576bddb81eec364cc01f32439f51d96c
Marcus Johnsson <mr.marcus.marega@gmail.com>
2020-03-13 09:48:47 +0100

    Improved UI

--- GlazerCalc/GlazerCalc.cs
+++ GlazerCalc/GlazerCalc.cs
@@ -5,8 +5,15 @@ class GlazerCalc
    static void Main()
     {
         double width, height, woodLength, glassArea;
-        string widthString, heightString, res;
+        string greeting, widthString, heightString, res;
 
+        greeting =
+@"The app prompts for two variables. width and height.
+Then process and print out the amount of wood and glass
+materials required to make a double paned window that
+will fit the dimensions provided.
+::::::::::::::::::::::::::::::::::::::::::::::::::::::";
+        Console.WriteLine(greeting);
         Console.WriteLine("Please state the width:");
         widthString = Console.ReadLine();
         width = double.Parse(widthString);
@@ -25,3 +32,12 @@ class GlazerCalc
         Console.WriteLine(res);
     }
 }
+
+// ÖVINING : Frågeställning
+
+// 1)  Var initieras variabeln heightString?
+//     Svar: heightString = Console.ReadLine();
+// 2)  Var sker det en konkatenering i programmet?
+//     Svar: Där res initieras.
+// 3)  Vilka operatorer kan vi känna igen i programmet?
+//     Svar: *, +, 
